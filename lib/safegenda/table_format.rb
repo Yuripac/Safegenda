acd5003bef362019c2f35d690ac5ce9119807314
Yuri <yuri_dias_@hotmail.com>
2015-06-13 13:28:28 -0300

    all hard tabs removed

--- lib/safegenda/table_format.rb
+++ lib/safegenda/table_format.rb
@@ -1,20 +1,20 @@
 module Safegenda
-	class TableFormat
+  class TableFormat
 
-		def self.create_row(activity, time = nil)
-			time = time || DEFAULT_TIME
+    def self.create_row(activity, time = nil)
+      time = time || DEFAULT_TIME
 
-			max_space  = 24
-			space_a = (max_space - activity.length)
-			space_t = (max_space - time.length)
-			
-			blank_space_a = " " * space_a
-			blank_space_t = " " * space_t
+      max_space  = 24
+      space_a = (max_space - activity.length)
+      space_t = (max_space - time.length)
+      
+      blank_space_a = " " * space_a
+      blank_space_t = " " * space_t
 
-			row =  "|  #{activity + blank_space_a}"
-			row << "|  #{time     + blank_space_t}|\n"
-			row << "|#{'-' * (row.length - 3)}|\n"		
-		end
-	
-	end
+      row =  "|  #{activity + blank_space_a}"
+      row << "|  #{time     + blank_space_t}|\n"
+      row << "|#{'-' * (row.length - 3)}|\n"    
+    end
+  
+  end
 end
