f9b9d281b9fac5119dc3a2eb636af33cc0853290
Yuri <yuri_dias_@hotmail.com>
2015-06-13 14:04:43 -0300

    all hard tabs removed

--- lib/safegenda/table_format.rb
+++ lib/safegenda/table_format.rb
@@ -13,7 +13,7 @@ module Safegenda
 
       row =  "|  #{activity + blank_space_a}"
       row << "|  #{time     + blank_space_t}|\n"
-      row << "|#{'-' * (row.length - 3)}|\n"    
+      row << "|#{'-' * (row.length - 3)}|\n"
     end
 
   end
